ee67625dc64693a2f9212af1ea0ab3dc8f5e4cb5
Kevin Lin <kevin.lin@zuhlke.com>
2026-03-02 14:34:59 +0800

    feat: Add a subcommand and options

--- Program.cs
+++ Program.cs
@@ -8,33 +8,52 @@ class Program
     {
         Option<FileInfo> fileOption = new("--file")
         {
-            Description = "The file to read and display on the console"
+            Description = "The file to read and display on the console",
+            Required = true
+        };
+        Option<int> delayOption = new("--delay")
+        {
+            Description = "Delay between lines, specified as milliseconds per character in a line",
+            DefaultValueFactory = parseResult => 42
+        };
+        Option<ConsoleColor> fgcolorOption = new("--fgcolor")
+        {
+            Description = "Foreground color of text displayed on the console",
+            DefaultValueFactory = parseResult => ConsoleColor.White
+        };
+        Option<bool> lightModeOption = new("--light-mode")
+        {
+            Description = "Background color of text displayed on the console: default is black, light mode is white"
         };
 
         RootCommand rootCommand = new("Sample app for System.CommandLine");
-        rootCommand.Options.Add(fileOption);
 
-        rootCommand.SetAction(parseResult =>
+        Command readCommand = new("read", "Read and display the file.")
         {
-            FileInfo? parsedFile = parseResult.GetValue(fileOption);
-            if (parsedFile is null)
-            {
-                Console.Error.WriteLine("Error: --file option is required.");
-                return 1;
-            }
-            ReadFile(parsedFile);
-            return 0;
-        });
+            fileOption,
+            delayOption,
+            fgcolorOption,
+            lightModeOption
+        };
+        rootCommand.Subcommands.Add(readCommand);
 
-        ParseResult parseResult = rootCommand.Parse(args);
-        return parseResult.Invoke();
+        readCommand.SetAction(parseResult => ReadFile(
+            parseResult.GetValue(fileOption)!,
+            parseResult.GetValue(delayOption),
+            parseResult.GetValue(fgcolorOption),
+            parseResult.GetValue(lightModeOption)));
+
+        return rootCommand.Parse(args).Invoke();
     }
 
-    static void ReadFile(FileInfo file)
+    internal static void ReadFile(FileInfo file, int delay, ConsoleColor fgColor, bool lightMode)
     {
+        Console.BackgroundColor = lightMode ? ConsoleColor.White : ConsoleColor.Black;
+        Console.ForegroundColor = fgColor;
         foreach (string line in File.ReadLines(file.FullName))
         {
             Console.WriteLine(line);
+            Thread.Sleep(TimeSpan.FromMilliseconds(delay * line.Length));
         }
     }
 }
